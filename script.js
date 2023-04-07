1478d355eb9c7e6d1a6c12259530c2c4da9bc5e1
Aurore Chevin <chevinaurore@gmail.com>
2023-04-07 17:40:25 +0200

    modif table

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-let article1 ={id:1, name: "S10", brand: "Samsung", price: 200, quantity: 1,category: "Smartphone"};
-let article2 ={id:2,name: "Macbook", brand: "Apple", price: 2000, quantity: 1,category: "PC"};
-let article3 ={id:3,name: "Zenpad", brand: "Asus", price: 300, quantity: 1,category: "Tablet"};
-let article4 ={id:4,name: "S20", brand: "Samsung", price: 601, quantity: 1,category: "Smartphone"};
-let article5 ={id:5,name: "P30", brand: "Huawei", price: 235, quantity: 1,category: "Smartphone"};
-let article6 ={id:6,name: "Galaxy Tab A7", brand: "Samsung", price: 169, quantity: 1,category: "Tablet"};
-let article7 ={id:7,name: "TUF A17", brand: "Asus", price: 1199, quantity: 1,category: "PC"};
-/*let listArticle=new Map();
-listArticle.set(1, {id:1,name: 'S10', brand: 'Samsung', price: 200, quantity: 1,category: 'Smartphone'});
-listArticle.set(2, {id:2,name: 'Macbook', brand: 'Apple', price: 2000, quantity: 1,category: 'PC'});
-listArticle.set(3, {id:3,name: 'Zenpad', brand: 'Asus', price: 300, quantity: 1,category: 'Tablet'});
-listArticle.forEach(tableContents => console.log(tableContents));*/
+class Article{
+    constructor(id,name,brand,price,quantity,category){
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+    }
+}
+let article1 = new Article(1, 'S10', 'Samsung', 200, 1, 'Smartphone');
+let article2 = new Article(2, 'Macbook', 'Apple', 2000, 1, 'PC');
+let article3 = new Article(3, 'Zenpad', 'Asus', 300, 1, 'Tablet');
+let article4 = new Article(4, 'S20', 'Samsung', 601, 1, 'Smartphone');
+let article5 = new Article(5, 'P30', 'Huawei', 235, 1, 'Smartphone');
+let article6 = new Article(6, 'Galaxy Tab A7', 'Samsung', 169, 1, 'Tablet');
+let article7 = new Article(7, 'TUF A17', 'Asus', 1199, 1, 'PC');
+/*let listArticle2=new Map();
+listArticle2.set(1, new Article(1, 'S10', 'Samsung', 200, 1, 'Smartphone'));
+listArticle2.set(2, {id:2,name: 'Macbook', brand: 'Apple', price: 2000, quantity: 1,category: 'PC'});
+listArticle2.set(3, {id:3,name: 'Zenpad', brand: 'Asus', price: 300, quantity: 1,category: 'Tablet'});
+listArticle2.forEach(tableContents => console.log(tableContents));*/
 let listArticle=new Array();
     listArticle.push([article1.id, article1.name, article1.brand, article1.price, article1.quantity, article1.category]);
     listArticle.push([article2.id, article2.name, article2.brand, article2.price, article2.quantity, article2.category]);
@@ -21,7 +31,7 @@ let listArticle=new Array();
 function createTable() {
     let tableContents = "";
         for (let i = 0; i < listArticle.length; i++) {
-            tableContents += "<tr>";
+            row = document.createElement("tr");
             for (let j = 0; j < listArticle[i].length; j++) {
                 tableContents += "<td>" + listArticle[i][j] + "</td>";
             }
@@ -30,3 +40,4 @@ function createTable() {
         document.getElementById("Articles").innerHTML +=tableContents;
         listArticle.forEach(tableContents => console.log(tableContents));
  }
+ 
